d870ae4adcbe2bfc33765b740c4b8ac8c03f199e
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2024-12-13 10:36:18 -0500

    Remove unused cli options

--- src/cli.rs
+++ src/cli.rs
@@ -5,17 +5,6 @@ use clap::{Parser, Subcommand};
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 pub struct Cli {
-    /// Optional name to operate on
-    pub name: Option<String>,
-
-    /// Sets a custom config file
-    #[arg(short, long, value_name = "FILE")]
-    pub config: Option<PathBuf>,
-
-    /// Turn debugging information on
-    #[arg(short, long, action = clap::ArgAction::Count)]
-    pub debug: u8,
-
     #[command(subcommand)]
     pub command: Commands,
 }
